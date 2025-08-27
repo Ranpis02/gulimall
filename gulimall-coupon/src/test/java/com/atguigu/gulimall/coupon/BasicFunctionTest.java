5799dbdf095a95d3c25f6809a66cf86c82064e58
Zhihong Xie <ranpis02@gmail.com>
2025-08-27 16:20:54 +0800

    :fire: Remove some unused imports

--- gulimall-coupon/src/test/java/com/atguigu/gulimall/coupon/BasicFunctionTest.java
+++ gulimall-coupon/src/test/java/com/atguigu/gulimall/coupon/BasicFunctionTest.java
@@ -1,15 +1,12 @@
 package com.atguigu.gulimall.coupon;
 
-import com.atguigu.common.utils.Query;
 import com.atguigu.gulimall.coupon.entity.SkuLadderEntity;
 import com.atguigu.gulimall.coupon.service.SkuLadderService;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
-import java.sql.Wrapper;
 
 @SpringBootTest
 public class BasicFunctionTest {
